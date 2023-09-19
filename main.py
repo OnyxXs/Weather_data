37a521831ff01d30a60552395dd5f850b01759de
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-09-19 16:34:49 +0200

    merge de main

--- main.py
+++ main.py
@@ -7,9 +7,4 @@ app = FastAPI()
 # Chargement des données météorologiques depuis un fichier JSON
 with open("rdu-weather-history.json", "r") as file:
     weather_data = json.load(file)
-
-
-
-
-
-
+    
