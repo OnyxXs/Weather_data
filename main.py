b2fce58b04bc6a3cd8ad330729fe2f20d1bf3257
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-09-19 10:31:20 +0200

    Use "http://127.0.0.1:8000/all-weather-data"

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 import json
-from typing import List
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+
 app = FastAPI()
 
 # Chargement des données météorologiques depuis un fichier JSON
 with open("rdu-weather-history.json", "r") as file:
     weather_data = json.load(file)
 
-
-    """
-    Obtenez des données météorologiques filtrées en fonction des dates spécifiées.
-
-    Args:
-        start_date (str): Date de début pour la filtration.
-        end_date (str): Date de fin pour la filtration.
-
-    Returns:
-        List[dict]: Liste des données météorologiques correspondant à la plage de dates spécifiée.
-    """
-
-
-@app.get("/weather")
-async def get_weather_filter_date(
-    start_date: str = Query(..., description="Date de début : "),
-    end_date: str = Query(..., description="Date de fin : ")
-) -> List[dict]:
-    # Filtrer les données en fonction des dates spécifiées
-    date_data = [data for data in weather_data if start_date <= data["date"] <= end_date]
-    return date_data
+# Endpoint pour obtenir toutes les données météorologiques
+@app.get("/all-weather-data")
+async def get_all_weather_data():
+    return weather_data
