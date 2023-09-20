7565892e7935457a62f30eb607ac8385da7919a1
Noé HLM <noeheylemadec@gmail.com>
2023-09-20 14:37:21 +0200

    better connection to bdd

--- database/test_connect.py
+++ database/test_connect.py
@@ -39,7 +39,7 @@ async def shutdown_db_client():
     await database.disconnect()
 
 
-@app.get("/notes/", response_model=List[Country])
+@app.get("/country/", response_model=List[Country])
 async def read_country():
     query = select([country])
     results = await database.fetch_all(query)
