787682928392e77809ac62b5677acaf50520000e
Noé HLM <noeheylemadec@gmail.com>
2023-09-22 10:06:32 +0200

    refactor and pagination

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from routers.show_country import router_show_country
 from routers.weather_by_date import router_weather_by_date
 from routers.create_date import router_create_date
 from routers.modif_date import router_modif_date
-from routers.delete_date import router_delete_date
+from routers.suppr_date import router_suppr_date
 from routers.order_by_prpc import router_order_by_prpc
 from routers.order_by_tmax import router_order_by_tmax
 from routers.order_by_tmin import router_order_by_tmin
