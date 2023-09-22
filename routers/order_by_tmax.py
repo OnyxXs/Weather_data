42163c059fb974b64e1df11a44574f59316f5826
OnyxXs <timeo.donnaes@gmail.com>
2023-09-22 09:30:47 +0200

    comments on : order_by_prpc.py, order_by_tmax.py and order_by_tmin.py

--- routers/order_by_tmax.py
+++ routers/order_by_tmax.py
@@ -1,20 +1,42 @@
 from fastapi import HTTPException, APIRouter
 from database.database import connect_to_database, close_database_connection
 
+# Création d'un routeur FastAPI pour la gestion des requêtes liées à l'ordre par Tmax
 router_order_by_tmax = APIRouter()
-
 
 @router_order_by_tmax.get('/order_by_tmax')
 async def order_by_tmax():
+    """
+    Gère la requête GET pour obtenir des données météorologiques triées par Tmax (température maximale).
+
+    Raises:
+        HTTPException: En cas d'erreur, renvoie une réponse HTTP appropriée avec un code d'erreur.
+
+    Returns:
+        dict: Un dictionnaire contenant les données météorologiques triées par Tmax et leur date.
+    """
     try:
+        # Établit une connexion à la base de données
         conn, cursor = connect_to_database()
+
+        # Exécute une requête SQL pour sélectionner les données météorologiques dans la table 'temp' triées par Tmax
         cursor.execute(
-            "SELECT date,Tmax FROM temp ORDER BY Tmax"
+            "SELECT date, Tmax FROM temp ORDER BY Tmax"
         )
+
+        # Récupère les résultats de la requête
         order_tmax = cursor.fetchall()
+
+        # Ferme la connexion à la base de données
         close_database_connection()
+
+        # Si aucune donnée n'a été trouvée, lève une exception HTTP avec un code 404
         if not order_tmax:
-            raise HTTPException(status_code=404, detail="Aucune données météorologiques")
+            raise HTTPException(status_code=404, detail="Aucune donnée météorologique trouvée")
+
+        # Retourne les données météorologiques triées par Tmax et leur date sous forme de dictionnaire
         return {"date_order_tmax": order_tmax}, 200
+
     except Exception as e:
+        # En cas d'erreur non prévue, lève une exception HTTP avec un code 500 et détaille l'erreur
         raise HTTPException(status_code=500, detail=str(e))
--- routers/order_by_tmin.py
+++ routers/order_by_tmin.py
@@ -1,20 +1,42 @@
 from fastapi import HTTPException, APIRouter
 from database.database import connect_to_database, close_database_connection
 
+# Création d'un routeur FastAPI pour la gestion des requêtes liées à l'ordre par Tmin
 router_order_by_tmin = APIRouter()
-
 
 @router_order_by_tmin.get('/order_by_tmin')
 async def order_by_tmin():
+    """
+    Gère la requête GET pour obtenir des données météorologiques triées par Tmin (température minimale).
+
+    Raises:
+        HTTPException: En cas d'erreur, renvoie une réponse HTTP appropriée avec un code d'erreur.
+
+    Returns:
+        dict: Un dictionnaire contenant les données météorologiques triées par Tmin et leur date.
+    """
     try:
+        # Établit une connexion à la base de données
         conn, cursor = connect_to_database()
+
+        # Exécute une requête SQL pour sélectionner les données météorologiques dans la table 'temp' triées par Tmin
         cursor.execute(
-            "SELECT date,Tmin FROM temp ORDER BY Tmin"
+            "SELECT date, Tmin FROM temp ORDER BY Tmin"
         )
+
+        # Récupère les résultats de la requête
         order_tmin = cursor.fetchall()
+
+        # Ferme la connexion à la base de données
         close_database_connection()
+
+        # Si aucune donnée n'a été trouvée, lève une exception HTTP avec un code 404
         if not order_tmin:
-            raise HTTPException(status_code=404, detail="Aucune données météorologiques")
+            raise HTTPException(status_code=404, detail="Aucune donnée météorologique trouvée")
+
+        # Retourne les données météorologiques triées par Tmin et leur date sous forme de dictionnaire
         return {"date_order_tmin": order_tmin}, 200
+
     except Exception as e:
+        # En cas d'erreur non prévue, lève une exception HTTP avec un code 500 et détaille l'erreur
         raise HTTPException(status_code=500, detail=str(e))
