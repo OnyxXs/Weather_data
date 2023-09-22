fc4f76dd127d0089f656e4fc673ae50cffd1e3d9
Noé HLM <noeheylemadec@gmail.com>
2023-09-22 09:24:12 +0200

    docstrings average files

--- routers/average_max_temp.py
+++ routers/average_max_temp.py
@@ -6,6 +6,21 @@ router_average_max_temp = APIRouter()
 
 @router_average_max_temp.get('/average_max_temp')
 async def average_max_temp(city_id: int = Query(..., description="ID de la ville pour la moyenne Tmax")):
+    """
+    Calcule la moyenne des températures maximales (Tmax) pour une ville spécifiée.
+
+    Args:
+        city_id (int): L'identifiant de la ville pour laquelle calculer la moyenne Tmax.
+
+    Returns:
+        dict: Un dictionnaire contenant la moyenne des températures maximales calculée.
+            - "average_max_temp" (float): La moyenne des températures maximales (Tmax).
+
+    Raises:
+        HTTPException:
+            - 404: Si aucune donnée météorologique n'est disponible pour la ville spécifiée.
+            - 500: Si une erreur inattendue se produit lors de l'accès à la base de données.
+    """
     try:
         conn, cursor = connect_to_database()
         cursor.execute(
@@ -15,7 +30,7 @@ async def average_max_temp(city_id: int = Query(..., description="ID de la ville
         data_average_max_temp = cursor.fetchone()
         close_database_connection()
         if not data_average_max_temp:
-            raise HTTPException(status_code=404, detail="Aucune données météorologiques pour la ville spécifiée")
+            raise HTTPException(status_code=404, detail="Aucune donnée météorologique pour la ville spécifiée")
         return {"average_max_temp": data_average_max_temp['average_max_temp']}, 200
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
